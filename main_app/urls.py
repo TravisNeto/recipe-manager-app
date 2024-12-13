8e6b09450abb02044a5f05b85e59a5736cb96d87
travisneto <travisneto@live.com>
2024-12-12 20:46:11 -0500

    Worked on Logout but having issues

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('signup/', views.signup, name='signup'),
     path('', views.home, name='home'),
+    
 
     #Recipe
     path('recipes/', views.RecipeList.as_view(), name='recipe-list'),
